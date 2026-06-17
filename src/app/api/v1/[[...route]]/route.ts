e879e0fd94c2bbf07d210198e011739af8f78c3a
Diego Vilhalva <89986274+diegovilhalva@users.noreply.github.com>
2026-06-17 08:52:07 -0300

    Change paddleRoute to root endpoint

--- src/app/api/v1/[[...route]]/route.ts
+++ src/app/api/v1/[[...route]]/route.ts
@@ -9,7 +9,7 @@ const app = new Hono().basePath('/api/v1')
 
 app.route("/files",fileRoute)
 
-app.route("/webhook",paddleRoute)
+app.route("/",paddleRoute)
 
 export const GET = handle(app)
 export const POST = handle(app)
